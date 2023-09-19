982d584be7b2d787639815328f9da3835bebe645
Y.kazama <Y.Kazama1996@gmail.com>
2023-09-19 14:26:17 +0800

    delete 4 and 5 and update feature 1

--- src/main/java/com/kazama/springbootcicd/controller/TestController.java
+++ src/main/java/com/kazama/springbootcicd/controller/TestController.java
@@ -10,22 +10,12 @@ public class TestController {
 
     @GetMapping("/feature1")
     public String feature1() {
-        return "I'm feature 1";
+        return "I'm feature update 1";
     }
 
     @GetMapping("/feature2")
     public String feature2() {
         return "Hello This is feature 2";
-    }
-
-    @GetMapping("/feature4")
-    public String feature4() {
-        return " Hello This is feature 4 ~~~~";
-    }
-
-    @GetMapping("/feature5")
-    public String feature5() {
-        return " Hello This is feature 5 ~~~~";
     }
 
 }
